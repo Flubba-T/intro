d05313bc54aa6092b357bd89b845c70c6aa45027
Flubbat <37635541+Flubbat@users.noreply.github.com>
2018-03-26 11:52:21 -0500

    Update helloWorld.py

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,11 @@
-print ('Hello, World!')
+print('Hello, World!')
+myint = 7
+print(myint)
+myfloat = 7.0
+print(myfloat)
+myfloat = float(7)
+print(myfloat)
+one = 1
+two = 2
+three = one + two
+print(three)
